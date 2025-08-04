8a5a43dffa8e8548358543b8186af062c9ee9e9c
ArtificialIntellicat <12598939+ArtificialIntellicat@users.noreply.github.com>
2025-08-04 16:36:35 +0200

    Changed location of home view

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Dashboard');
+    return Inertia::render('CountryMap');
 })->name('home');
 
 Route::get('dashboard', function () {
